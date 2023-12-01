3088278588cb98fc9bc710ede95d629c20a58d35
Kaspar Moberg <kaspeti123@gmail.com>
2023-12-01 15:32:55 +0100

    added comments to day1 star1

--- day1/main.go
+++ day1/main.go
@@ -15,7 +15,12 @@ func main() {
     }
     defer f.Close()
 
-    scanner := bufio.NewScanner(f)
+    fmt.Printf("Solution to star one: %v", starone(f))
+}
+
+
+func starone(input *os.File) int {
+    scanner := bufio.NewScanner(input)
 
     nums := make([]int, 0)
     for scanner.Scan() {
@@ -24,6 +29,7 @@ func main() {
         for _, r := range scanner.Text() {
             // Check if rune is a digit between 0 and 9
             if r >= 48 && r <= 57 {
+                // Convert from unicode to integer representation
                 if num1 == ' ' {
                     num1 = r - 48
                 }             
@@ -31,6 +37,7 @@ func main() {
             }
         }
 
+        // Concatenate the numbers by multiplying the first by ten and then adding the remaining
         nums = append(nums, (int(num1) * 10) + int(num2))
     }
 
@@ -39,5 +46,5 @@ func main() {
         total += num
     }
 
-    fmt.Println(total)
+    return total
 }

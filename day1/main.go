be3c296d7a3f02240738c223b26aad307a106f9d
kaspar moberg <kaspeti123@gmail.com>
2023-12-01 22:56:02 +0100

    changed starone

--- day1/main.go
+++ day1/main.go
@@ -9,36 +9,45 @@ import (
 
 
 func main() {
+    fmt.Printf("Solution to star one: %v", starone())
+}
+
+
+func starone() int {
     f, err := os.Open("input")
     if err != nil {
         log.Fatal(err)
     }
     defer f.Close()
 
-    fmt.Printf("Solution to star one: %v", starone(f))
-}
-
-
-func starone(input *os.File) int {
-    scanner := bufio.NewScanner(input)
+    scanner := bufio.NewScanner(f)
 
     nums := make([]int, 0)
     for scanner.Scan() {
-        num1, num2 := ' ', ' '
+        num1, num2 := -1, -1
+        line := scanner.Text()
 
         for _, r := range scanner.Text() {
             // Check if rune is a digit between 0 and 9
             if r >= 48 && r <= 57 {
                 // Convert from unicode to integer representation
-                if num1 == ' ' {
-                    num1 = r - 48
-                }             
-                num2 = r - 48
+                num1 = int(r) - 48
+                break
+            }
+        }
+
+        for i := len(line)-1; i >= 0; i-- {
+            r := line[i]
+            // Check if rune is a digit between 0 and 9
+            if r >= 48 && r <= 57 {
+                // Convert from unicode to integer representation
+                num2 = int(r) - 48
+                break
             }
         }
 
         // Concatenate the numbers by multiplying the first by ten and then adding the remaining
-        nums = append(nums, (int(num1) * 10) + int(num2))
+        nums = append(nums, (num1 * 10) + num2)
     }
 
     total := 0
@@ -48,3 +57,4 @@ func starone(input *os.File) int {
 
     return total
 }
+

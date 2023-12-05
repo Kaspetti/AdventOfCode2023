6ee52c200b637a968f490798b3c7cb7a6b2f8439
Kaspar Moberg <kaspeti123@gmail.com>
2023-12-05 14:53:25 +0100

    completed day 3 star 2

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ Solutions for 2023 Advent of Code. Each day is a random programming language. Wh
 |-----------------|----------|---------------|
 |  [Day 1](day1)  |    Go    | :star: :star: |
 |  [Day 2](day2)  | NuShell  | :star: :star: |
-| Day 3           | Haskell  | :star:        |
-| Day 4 |  |  |
-| Day 5 |  |  |
+|  [Day 3](day3)  | Haskell  | :star: :star: |
+| Day 4           |    Go    |               |
+| Day 5           | NuShell  |               |
 | Day 6 |  |  |
 | Day 7 |  |  |
 | Day 8 |  |  |
--- day3/main.hs
+++ day3/main.hs
@@ -44,8 +44,61 @@ isSymbolNeighbour (x, y) symbolGrid =
   fromMaybe False (lookup (x-1, y-1) symbolGrid)
 
 
+type NumbersGrid = [((Int, Int), Int)]
+
+getNumbersGrid :: String -> NumbersGrid
+getNumbersGrid input = getNumbersGrid' input "" 0 0 
+  where
+    getNumbersGrid' :: String -> String -> Int -> Int -> NumbersGrid
+    getNumbersGrid' [] curNum x y = [((x-1, y), fromMaybe 0 (readMaybe curNum)) | not (null curNum)]
+    getNumbersGrid' (c:cs) curNum x y | isDigit c = getNumbersGrid' cs (curNum++[c]) (x+1) y
+                                      | c == '\n' = 
+                                        if not (null curNum) then
+                                          ((x-1, y), fromMaybe 0 (readMaybe curNum)) : getNumbersGrid' cs "" 0 (y+1)
+                                        else
+                                          getNumbersGrid' cs "" 0 (y+1)
+                                      | otherwise = if not (null curNum) then
+                                          ((x-1, y), fromMaybe 0 (readMaybe curNum)) : getNumbersGrid' cs "" (x+1) y
+                                        else
+                                          getNumbersGrid' cs "" (x+1) y
+
+
+getGearRatios :: String -> NumbersGrid -> Int
+getGearRatios input numbersGrid = getGears input numbersGrid 0 0
+  where
+    getGears :: String -> NumbersGrid -> Int -> Int -> Int
+    getGears [] _ _ _ = 0
+    getGears (c:cs) numbersGrid x y | c == '*' = 
+                                      let neighbours = getNumberNeighbours (x, y) numbersGrid
+                                      in
+                                        if length neighbours == 2 then
+                                          product neighbours + getGears cs numbersGrid (x+1) y
+                                        else
+                                          getGears cs numbersGrid (x+1) y
+                                    | c == '\n' = getGears cs numbersGrid 0 (y+1)
+                                    | otherwise = getGears cs numbersGrid (x+1) y
+
+
+getNumberNeighbours :: (Int, Int) -> NumbersGrid -> [Int]
+getNumberNeighbours (x, y) numbersGrid = getNumberNeighbours' (x, y) (-1) (-1) numbersGrid
+  where
+    getNumberNeighbours' :: (Int, Int) -> Int -> Int -> NumbersGrid -> [Int]
+    getNumberNeighbours' (x, y) 4 cy numbersGrid = getNumberNeighbours' (x, y) (-1) (cy+1) numbersGrid
+    getNumberNeighbours' _ _ 2 _ = []
+    getNumberNeighbours' (x, y) cx cy numbersGrid | cx == 0 && cy == 0 = getNumberNeighbours' (x, y) 1 cy numbersGrid
+                                                  | otherwise =
+                                                      let neighbour = fromMaybe (-1) (lookup (x + cx, y + cy) numbersGrid)
+                                                      in
+                                                        if neighbour == -1 || length (show neighbour) < cx then
+                                                          getNumberNeighbours' (x, y) (cx+1) cy numbersGrid
+                                                        else
+                                                          neighbour : getNumberNeighbours' (x, y) (cx+1) cy numbersGrid
+
 
 main :: IO()
 main = do
   input <- readFile "input"
-  print $ sumParts input (getSymbolGrid input)
+  let symbolGrid = getSymbolGrid input
+  print $ sumParts input symbolGrid
+  let numbersGrid = getNumbersGrid input
+  print $ getGearRatios input numbersGrid
